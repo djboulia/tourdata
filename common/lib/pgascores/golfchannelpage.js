e85f77bd145c666e23c15d9e5049ab5f1b9c8a6b
Don Boulia <djboulia@gmail.com>
2019-07-31 19:52:21 -0400

    new changes to pga data

--- common/lib/pgascores/golfchannelpage.js
+++ common/lib/pgascores/golfchannelpage.js
@@ -1,52 +1,136 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var getTournamentData = function ($) {
-    // get all script tags, look for the tournament data and parse it
-    var scripts = $('script');
-    if (scripts == undefined) {
-        console.log("Couldn't find scripts!");
-        callback(null);
-        return;
-    } else {
-        // console.log("found scripts");
-    }
+var searchString = "__gc_tournament_data__ = ";
 
-    // all event info now stored in this variable in the web page
-    var searchString = "__gc_tournament_data__ = ";
-    var text = scripts.text();
-    var start = text.indexOf(searchString);
-    var end = text
-        .substr(start + searchString.length)
-        .indexOf('";');
-
-    if (start < 0) {
-        console.log("Couldn't find tournament data!!");
-        callback(null);
-        return;
-    }
-
-    // the tournament data is stored as a string in a script with
-    // all sorts of escaped characters.  A normal JSON.parse doesn't seem
-    // to process it.  I think there's an encoding/conversion issue somewhere
-    // To fix, we munge it back by converting unicode numbers and special characters
-    var tournament_string = text.substr(start + searchString.length + 1, end - 1);
-
-    // console.log(tournament_string.substr(0, 30) + "..." + tournament_string.substr(-30));
-
+// the tournament data is stored as a string in a script with
+// all sorts of escaped characters.  A normal JSON.parse doesn't seem
+// to process it.  I think there's an encoding/conversion issue somewhere
+// To fix, we munge it back by converting unicode numbers and special characters
+var unescapeString = function (string) {
     var r = /\\u([\d\w]{4})/gi;
-    tournament_string = tournament_string.replace(r, function (match, grp) {
+    string = string.replace(r, function (match, grp) {
         return String.fromCharCode(parseInt(grp, 16));
     });
+
     var r = /\\([–’‘èéúíáñö“”\\\/])/gi;
-    tournament_string = tournament_string.replace(r, function (match, grp) {
+    string = string.replace(r, function (match, grp) {
         return grp;
     });
+
+    return string;
+};
+
+var findTournamentDataScript = function ($) {
+    var result = undefined;
+
+    var scripts = $('script');
+    if (scripts != undefined) {
+        // search each script tag for the right data
+        console.log("found " + $(scripts).length + " scripts!");
+
+        $(scripts).each(function (i, elem) {
+            var script = $(this).text();
+
+            var start = script.indexOf(searchString);
+            if (start >= 0) {
+                console.log("found tournament script!");
+                result = script;
+                return false;
+            }
+
+            return true;
+        });
+
+    }
+
+    return result;
+};
+
+var isVersion1 = function (script) {
+    var start = script.indexOf(searchString);
+
+    var nextChar = script
+        .substr(start + searchString.length, 1);
+
+    // console.log("isVersion1: " + nextChar);
+
+    if (nextChar === '"') {
+        return true;
+    }
+
+    return false;
+};
+
+var isVersion2 = function (script) {
+    var start = script.indexOf(searchString);
+
+    var nextChar = script
+        .substr(start + searchString.length, 1);
+
+    // console.log("isVersion2: " + nextChar);
+
+    if (nextChar === "{") {
+        return true;
+    }
+
+    return false;
+};
+
+var getTournamentData = function ($) {
+    // get all script tags, look for the tournament data and parse it
+    var script = findTournamentDataScript($);
+    if (script === undefined) {
+        console.log("Couldn't find tournament data script!");
+        return null;
+    }
+
+    var tournament_string = undefined;
+
+    if (isVersion1(script)) {
+        console.log("version 1 format detected");
+
+        // all event info now stored in this variable in the web page
+        var start = script.indexOf(searchString);
+        var end = script
+            .substr(start + searchString.length)
+            .indexOf('";');
+
+        tournament_string = script.substr(start + searchString.length + 1, end - 1);
+    } else if (isVersion2(script)) {
+        // changed the format in the middle of the 2019 season... handle that here
+        console.log("version 2 format detected");
+
+        eval(script);
+        // console.log(JSON.stringify(__gc_tournament_data__));
+
+        if (__gc_tournament_data__ && __gc_tournament_data__.data) {
+            tournament_string = __gc_tournament_data__.data;
+        }
+    }
+
+    if (tournament_string === undefined) {
+        console.log("Bad format for tournament data!");
+        return null;
+    }
+
+
+    // console.log(tournament_string.substr(0, 30) + "..." + tournament_string.substr(-30));
+    tournament_string = unescapeString(tournament_string);
 
     // console.log(tournament_string.substr(0, 1000));
     // console.log("start " + start + " end " + end);
 
-    var tournament_data = JSON.parse(tournament_string);
+    var tournament_data = null;
+
+    try {
+        tournament_data = JSON.parse(tournament_string);
+    } catch (err) {
+        console.log(err);
+        console.log("tournament_string " + tournament_string);
+
+        tournament_data = null;
+    }
     return tournament_data;
 }
 
@@ -59,13 +143,21 @@ var processPage = function (page, cb) {
 
         var tournament_data = getTournamentData($);
 
-        cb(tournament_data);
+        if (tournament_data != null) {
+            cb(tournament_data);
+            return true;
+        } else {
+            cb(null);
+            return false;
+        }
+
     } else {
         //            console.log("Error retrieving page: " + JSON.stringify(response));
         console.log("Error retrieving page!");
         cb(null);
     }
 
+    return false;
 }
 
 //
@@ -80,13 +172,17 @@ var GolfChannelPage = function () {
         request.get(url, (error, response, body) => {
 
             if (error || response.statusCode != 200) {
-                console.error("Error retrieving page.  Response code: " + (response) ? response.statusCode : undefined) ;
+                console.error("Error retrieving page.  Response code: " + (response) ? response.statusCode : undefined);
                 console.error("Error message: " + JSON.stringify(error));
             } else {
                 page = body;
             }
 
-            processPage(page, cb);
+            var result = processPage(page, cb);
+
+            if (!result) {
+                console.error("Error parsing page " + url);
+            }
         });
 
     };
